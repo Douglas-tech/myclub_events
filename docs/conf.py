baa9b7ff9b765871ef81078831863505379fa58c
Douglas Holder <dougholder01@gmail.com>
2023-07-05 12:55:08 +0200

    Changed line 12 of conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ import os
 import sys
 import django
 sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Your_project_name.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myclub_website.settings'
 django.setup()
 
 project = 'Myclub website'
