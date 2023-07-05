8106747e05c24716de9e362759e6907054697c87
Douglas Holder <dougholder01@gmail.com>
2023-07-05 14:47:35 +0200

    Removed myclub_website.settings on line 12 of conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,6 @@ import os
 import sys
 import django
 sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'myclub_website.settings'
 os.environ['DJANGO_SETTINGS_MODULE'] = 'events.settings'
 django.setup()
 
